c64a6bf72bd374003fddda1cc4ab89297d9a9afd
Ali Majeed <ali.mmajeed@devsinc.com>
2022-07-21 21:16:15 +0500

    Day 2 completed

--- classes.rb
+++ classes.rb
@@ -1,38 +1,66 @@
-# puts 1.class
-# puts 1.class.superclass
-# puts 1.class.superclass.superclass
-# puts 2.class.superclass.superclass.superclass
+# # puts 1.class
+# # puts 1.class.superclass
+# # puts 1.class.superclass.superclass
+# # puts 2.class.superclass.superclass.superclass
 
-puts Integer.superclass
-puts Numeric.superclass
-puts Object.superclass
+# puts Integer.superclass
+# puts Numeric.superclass
+# puts Object.superclass
 
-puts '=' * 20
-puts Array.superclass
+# puts '=' * 20
+# puts Array.superclass
 
-def check_p; end
+# def check_p; end
 
-puts check_p.class
-puts check_p.class.superclass
+# puts check_p.class
+# puts check_p.class.superclass
 
-def splat_test(*arr)
-  puts arr
-end
+# def splat_test(*arr)
+#   puts arr
+# end
 
-splat_test(1, 2, 3, :ali, 'majeed')
+# splat_test(1, 2, 3, :ali, 'majeed')
 
-def test2(n1,n2,n3)
-  puts n1+n2+n3
-end
-a1 = [1, 2, 3]
-test2(*a1)
-module TestModule
-  class Test
-    def var_chk
-      a= 15
-      a
-    end
+# def test2(n1,n2,n3)
+#   puts n1+n2+n3
+# end
+# a1 = [1, 2, 3]
+# test2(*a1)
+# module TestModule
+#   class Test
+#     def var_chk
+#       a= 15
+#       a
+#     end
+#   end
+# end
+
+# puts TestModule::Test.var_chk
+
+puts 2.kind_of?(Float)
+
+class ClassA
+  def initialize(a)
+    puts "#{self.class} + intializers called"
+  end
+  def parent_method
+    puts "Parent class method"
+  end
+  def self.class_m
+    puts 'Class A method'
   end
 end
 
-puts TestModule::Test.new.var_chk
+class ClassB < ClassA
+  def initialize
+    super('B')
+    puts "#{self.class} + intializers called"
+  end
+  def parent_method
+    super
+  end
+end
+
+ClassB.class_m
+p [4, 8, 15, 16, 23, 42,42,15].count
+
--- closure.rb
+++ closure.rb
@@ -1,15 +1,95 @@
-def find_frequency(sentence, word)
-  sentence.downcase.split.count(word.downcase)
+# def find_frequency(sentence, word)
+#   sentence.downcase.split.count(word.downcase)
+# end
+
+# puts find_frequency('Do or do not... there is no try.',"do")
+# p [19,4,6,2,6,2,4,5,2,3,2].sort{|a,b| b<=> a}
+
+# def sort_string(string)
+#   string.split.sort{|a,b| a.length<=>b.length}.join(' ')
+# end
+# p sort_string("Sort words in a sentence")
+
+
+# string = "1 2 3 4 5 6 7 8 9"
+# puts string.gsub(" ","")
+
+# (1..5).inject(0)do |sum,i|
+#   p sum+i**3
+# end
+
+# def non_duplicated_values(values)
+#   values.select{ |v| values.count(v)==1}
+# end
+
+# def array_of_fixnums?(array)
+#   check = false
+#   array.each do |i|
+#     p 2.class.superclass.superclass
+#   end
+# end
+
+# p array_of_fixnums?([1,2,2,3,3,4,5])
+
+# def kaprekar?(k)
+#   no_of_digits = k.to_s.size
+#   square = (k ** 2).to_s
+
+#   second_half = square[-no_of_digits..-1]
+#   first_half = square.size.even? ? square[0..no_of_digits-1] : square[0..no_of_digits-2]
+
+#   k == first_half.to_i + second_half.to_i
+# end
+
+# p kaprekar?(91)
+
+# p 123.shuffle.join.to_i
+
+
+addition = lambda{|a,b| a+b}
+p addition.call(1,2)
+
+class Calculator
+  def add(a, b)
+    return a + b
+  end
 end
 
-puts find_frequency('Do or do not... there is no try.',"do")
-p [19,4,6,2,6,2,4,5,2,3,2].sort{|a,b| b<=> a}
+addition = Calculator.new.method("add")
+addition = addition.to_proc
+p addition.class
 
-def sort_string(string)
-  string.split.sort{|a,b| a.length<=>b.length}.join(' ')
+# Explicit block
+def calculate(a,b,statement)
+  statement.call(a,b)
 end
-p sort_string("Sort words in a sentence")
+
+p calculate(1,2,lambda {|a,b| "#{a} + #{b} = #{a+b}"})
+
+# implicit block
+def cal(a,b)
+  puts "Implicit block"
+  yield(a,b)
+end
+
+p cal(4,2){ |a, b| a - b }
+
+puts "Conversion"
+# implicit to explicit
+def im_block(a,b,&block)
+  block.call(1,2)
+end
+
+p im_block(1,2){|a,b| a*b}
+
+# explicit to implicit
+def ex_block(a,b)
+  yield a, b
+end
+
+p ex_block(1,2,&addition)
+
+puts lambda {}
+puts Proc.new {}
 
 
-string = "1 2 3 4 5 6 7 8 9"
-puts string.gsub(" ","")
